631d06114c01d38bd319e80885371f2533979678
Ryan Freitas <101230901+RyanFreitass@users.noreply.github.com>
2025-07-08 16:12:54 -0300

    update

---
+++ Golang-Do-Zero/pacotes.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func mainPacotes() {
+	const name, age = "Ryan", 22
+	fmt.Println(name, "is", age, ".")
+
+	fmt.Println(strings.Split("steph", ""))
+}
+
+// Tipos:
+// bool (TRUE / FALSE)
---
+++ Golang-Do-Zero/tipos.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Bool
+	//fmt.Printf("Type: %T - Value: %v\n", false, false)
+	
+	// String
+	fmt.Printf("Type: %T - Value: %v\n", "steph", "steph")
+	
+	//fmt.Printf("Type: %T - Value: %v\n", 1, 1)
+	//fmt.Printf("Type: %T - Value: %v\n", "1", "1")
+	//fmt.Printf("Type: %T - Value: %v\n", 1.233, 1.233)
+}
+
+// TIPOS:
+// bool (t/f)
+// string - sequencia de bytes
+// int
+// float (float64/float32) - decimal
