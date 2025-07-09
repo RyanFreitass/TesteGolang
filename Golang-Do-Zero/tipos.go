27e48390cca0387a8d82712888b5d99f102174ef
Ryan Freitas <101230901+RyanFreitass@users.noreply.github.com>
2025-07-09 13:59:53 -0300

    Create varconst.go and zerovalues.go

--- Golang-Do-Zero/tipos.go
+++ Golang-Do-Zero/tipos.go
@@ -4,16 +4,19 @@ import (
 	"fmt"
 )
 
-func main() {
+func mainTipos() {
 	// Bool
 	//fmt.Printf("Type: %T - Value: %v\n", false, false)
 	
 	// String
-	fmt.Printf("Type: %T - Value: %v\n", "steph", "steph")
-	
-	//fmt.Printf("Type: %T - Value: %v\n", 1, 1)
+	//fmt.Printf("Type: %T - Value: %v\n", "steph", "steph")
 	//fmt.Printf("Type: %T - Value: %v\n", "1", "1")
-	//fmt.Printf("Type: %T - Value: %v\n", 1.233, 1.233)
+
+	// Int
+	//fmt.Printf("Type: %T - Value: %v\n", 1, 1)
+
+	// Float
+	fmt.Printf("Type: %T - Value: %v\n", 1.233, 1.233)
 }
 
 // TIPOS:
---
+++ Golang-Do-Zero/varconst.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Váriaveis
+	// var + nome da variavel + tipo
+	var nome string
+	nome = "bento"
+	fmt.Println(nome)
+
+	nome = "steph"
+	fmt.Println(nome)
+
+	var idade int
+	idade = 4
+	fmt.Println(idade)
+
+	var b, c int = 2, 3
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	numero := 1
+	fmt.Println(numero)
+
+
+
+	// Constantes
+	const idadeBento = 4
+	fmt.Println(idadeBento)
+}
